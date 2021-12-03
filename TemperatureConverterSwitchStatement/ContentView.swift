f99b6be712d1ab3c8c4c53ae50efc49d686db43e
Lillian Yang <lyang@lcs.on.ca>
2021-12-03 11:30:38 -0500

    Separated the logic out into its own computed property. This keep body property clean and simple - which leads to better performance.

--- TemperatureConverterSwitchStatement/ContentView.swift
+++ TemperatureConverterSwitchStatement/ContentView.swift
@@ -13,10 +13,31 @@ struct ContentView: View {
     @State var temperatureInCelsius = 0.0
     
     // MARK: Computed properties
+    //Process the logic goes here
     var temperatureInFahrenheit: Double {
         return temperatureInCelsius * 1.8 + 32
     }
     
+    //To provide the suggestion on what to wear based upon the temperature
+    var feedback: String {
+        if temperatureInCelsius >= -50.0 && temperatureInCelsius < -20.0 {
+            return "It's freezing outside! 🥶"
+        } else if temperatureInCelsius >= -20.0 && temperatureInCelsius < 0.0 {
+            return "Dress warmly!"
+        } else if temperatureInCelsius >= 0.0 && temperatureInCelsius < 10.0 {
+            return "Need a coat!"
+        } else if temperatureInCelsius >= 10.0 && temperatureInCelsius < 20.0 {
+            return "Not very cold, not very hot... Perfect!"
+        } else if temperatureInCelsius >= 20.0 && temperatureInCelsius < 30.0 {
+            return "Warm weather! Wear thin layers."
+        } else {
+            return "Never go out! It's too hot. 🥵"
+        }
+    }
+    
+    
+    
+// "body property is ONLY for Input and Output
     var body: some View {
         
         VStack {
@@ -52,22 +73,6 @@ struct ContentView: View {
                     .bold()
                 
                 
-                
-                //Output: what to wear
-                if temperatureInCelsius >= -50.0 && temperatureInCelsius < -20.0 {
-                    Text("It's freezing outside! 🥶")
-                } else if temperatureInCelsius >= -20.0 && temperatureInCelsius < 0.0 {
-                    Text("Dress warmly!")
-                } else if temperatureInCelsius >= 0.0 && temperatureInCelsius < 10.0 {
-                    Text("Need a coat!")
-                } else if temperatureInCelsius >= 10.0 && temperatureInCelsius < 20.0 {
-                    Text("Not very cold, not very hot... Perfect!")
-                } else if temperatureInCelsius >= 20.0 && temperatureInCelsius < 30.0 {
-                    Text("Warm weather! Wear thin layers.")
-                } else if temperatureInCelsius >= 30.0 && temperatureInCelsius <= 50.0 {
-                    Text("Never go out! It's too hot. 🥵")
-                }
-                
                 Spacer()
             }
 
@@ -75,6 +80,11 @@ struct ContentView: View {
             Text("\(String(format: "%.1f", temperatureInFahrenheit)) °F")
                 .bold()
                 .padding()
+            
+            
+            //Output: what to wear
+            
+            Text(feedback)
             
             Spacer()
             
